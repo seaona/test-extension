88ee17e86d0849de7cbf5ef4e47e1cd73f7e555a
seaona <mariona@gmx.es>
2022-09-13 10:06:50 +0200

    Use metamask/providers instead of extension-provider and inpage-provider

--- README.md
+++ README.md
@@ -5,12 +5,11 @@ This is a simple extension that can be used for testing MetaMask connection usin
 
 ## Setup
 1. Install MetaMask on Chrome
-2. Copy MetaMask Extension ID from `chrome://extensions/`
-3. Install dependencies `cd extension npm i`
-4. Paste MetaMask Extension ID into the variable `METAMASK_EXTENSION_ID` from `src/main.js` file
-5. Build the extension `npm run extension`
-6. Import extension root folder into Chrome
-7. Click Connect to MetaMask
+2. Clon this repository
+3. Change to extension folder and install dependencies `cd extension npm i`
+4. Build the extension `npm run extension`
+5. Import extension root folder into Chrome
+6. Click Connect to MetaMask
 
 ## TODO
 - [ ] Connect to MetaMask using MV2
--- extension/src/main.js
+++ extension/src/main.js
@@ -1,29 +1,16 @@
-/*global chrome*/
-import Web3 from 'web3'
-import  { MetamaskInpageProvider } from 'metamask-inpage-provider'
-import PortStream from 'extension-port-stream'
+import { initializeProvider } from '@metamask/providers';
+import LocalMessageDuplexStream from 'post-message-stream';
 
+// Create a stream to a remote provider:
 export async function provider() {
-  const METAMASK_EXTENSION_ID = 'oejhglpjccoedjbbdaafbgajppbmicle';
-  const metamaskPort =chrome.runtime.connect(METAMASK_EXTENSION_ID);
-  const pluginStream = new PortStream(metamaskPort);
-  const web3Provider = new MetamaskInpageProvider(pluginStream);
-  const web3 = new Web3(web3Provider);
-
-  console.log(metamaskPort)
-  console.log(pluginStream)
-  console.log(web3Provider)
-  console.log(web3)
-
-  const chainId = JSON.stringify(web3Provider.chainId)
-  console.log(chainId)
+  const metamaskStream = new LocalMessageDuplexStream({
+    name: 'inpage',
+    target: 'contentscript',
+  });
   
-  const address = await web3.eth.getAccounts();
-  console.log(JSON.stringify(address))
-  await web3.eth.sendTransaction({                           
-      from: address[0],                           
-      to: '0x1C53dc20D1E36ed8359250dE626ACAe36BD28a29',
-      value: web3.utils.toWei('0.000001'),                        
-  });                      
-
+  // this will initialize the provider and set it as window.ethereum
+  initializeProvider({
+    connectionStream: metamaskStream,
+  });
+  console.log(window.ethereum)
 }
